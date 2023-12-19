e0cb964f84d7af1407c937ec75dc04ced9e859d4
alongstringofnumbers <sjensen793@gmail.com>
2023-12-19 16:56:26 -0700

    Some cleanup

--- src/main/java/gregtech/api/GTValues.java
+++ src/main/java/gregtech/api/GTValues.java
@@ -158,7 +158,8 @@ public class GTValues {
             MODID_TCON = "tconstruct",
             MODID_PROJRED_CORE = "projectred-core",
             MODID_RC = "railcraft",
-            MODID_CHISEL = "chisel";
+            MODID_CHISEL = "chisel",
+            MODID_LITTLETILES = "littletiles";
 
     private static Boolean isClient;
 
